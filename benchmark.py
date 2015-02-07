7320b0c909f343f08023485cfe59442d2c01c677
Andrew Zey <andrew@andrewzey.com>
2015-02-06 16:18:25 -0800

    refactor write benchmarks into benchmark class

--- benchmark.py
+++ benchmark.py
@@ -39,23 +39,45 @@ for x in range(0, 100000):
     csvseq.append(c)
 
 
+class Benchmark(object):
+
+    def write(self, format):
+        time_start = time.time()
+
+        if format == 'json':
+            open('./output/output.json', 'w').write(json.dumps(jsonseq, separators=(',', ':')))
+
+        elif format == 'protobuf':
+            open('./output/output.pb', 'wb').write(addressbook.SerializeToString())
+
+        elif format == 'gzjson':
+            gzip.open('./output/output.jsz', 'wb').write(json.dumps(jsonseq, separators=(',', ':')))
+
+        time_end = time.time()
+
+        return time_start - time_end
+
+
+    
+benchmark = Benchmark()
+
+
 # Write Benchmarks
 print 'Running write benchmarks...'
-t0 = time.time()
-open('./output/output.json', 'w').write(json.dumps(jsonseq, separators=(',', ':')))
-t1 = time.time()
-open('./output/output.pb', 'wb').write(addressbook.SerializeToString())
-t2 = time.time()
-gzip.open('./output/output.jsz', 'wb').write(json.dumps(jsonseq, separators=(',', ':')))
-t3 = time.time()
-out = csv.DictWriter(gzip.open('./output/output.csvz', 'wb'), csvseq[0].keys())
-out.writerows(csvseq)
-t4 = time.time()
+# t0 = time.time()
+# open('./output/output.json', 'w').write(json.dumps(jsonseq, separators=(',', ':')))
+# t1 = time.time()
+# open('./output/output.pb', 'wb').write(addressbook.SerializeToString())
+# t2 = time.time()
+# gzip.open('./output/output.jsz', 'wb').write(json.dumps(jsonseq, separators=(',', ':')))
+# t3 = time.time()
 
-json_write = t1 - t0
-proto_write = t2 - t1
-gzjson_write = t3 - t2
-gzcsv_write = t4 - t3
+# json_write = t1 - t0
+# proto_write = t2 - t1
+# gzjson_write = t3 - t2
+json_write   = benchmark.write('json')
+proto_write  = benchmark.write('protobuf')
+gzjson_write = benchmark.write('gzjson')
 
 # Read Benchmarks
 print 'Running read benchmarks...'
@@ -66,13 +88,10 @@ addressbook_pb2.AddressBook().ParseFromString(open('./output/output.pb', 'rb').r
 t2 = time.time()
 json.loads(gzip.open('./output/output.jsz', 'rb').read())
 t3 = time.time()
-list(csv.DictReader(gzip.open('./output/output.jsz', 'rb')))
-t4 = time.time()
 
 json_read = t1 - t0
 proto_read = t2 - t1
 gzjson_read = t3 - t2
-gzcsv_read = t4 - t3
 
 
 # File Size Benchmarks
@@ -80,7 +99,6 @@ print 'Running file size benchmarks...\n'
 json_size   = float(os.stat('./output/output.json').st_size)
 proto_size  = float(os.stat('./output/output.pb'  ).st_size)
 gzjson_size = float(os.stat('./output/output.jsz' ).st_size)
-gzcsv_size  = float(os.stat('./output/output.csvz').st_size)
 
 
 #Print Output
@@ -89,4 +107,3 @@ print '\t%s\t%s\t%s'               % ('Write', 'Read', 'Size')
 print 'json   \t%0.2f\t%0.2f\t%0.2f' % (json_write   / json_write, json_read   / json_read, json_size   / json_size)
 print 'proto  \t%0.2f\t%0.2f\t%0.2f' % (proto_write  / json_write, proto_read  / json_read, proto_size  / json_size)
 print 'json.gz\t%0.2f\t%0.2f\t%0.2f' % (gzjson_write / json_write, gzjson_read / json_read, gzjson_size / json_size)
-print 'csv.gz \t%0.2f\t%0.2f\t%0.2f' % (gzcsv_write  / json_write, gzcsv_read  / json_read, gzcsv_size  / json_size)
